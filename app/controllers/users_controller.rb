87ba4287279b04e3902b4a29abfbbe3b3b8b1952
Andrew-J-Williams <1williamsaj@gmail.com>
2020-02-12 20:00:28 -0600

    initialize git

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,42 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        erb :'users/signup'
+    end
+
+    post '/signup' do
+        post '/signup' do
+            @user = User.new(params)
+            if @user.save
+              session[:user_id] = @user.id
+              redirect to "/customers"
+            else
+              erb :'users/signup'
+            end
+        end
+    end
+
+    get '/login' do
+        erb :'users/login'
+    end
+
+    post '/login' do
+        @user = User.find_by(:email => params[:email])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect '/customers'
+          else
+            redirect '/signup'
+        end
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.clear
+            redirect to '/login'
+        else
+            redirect to '/'
+        end
+      end
 
 end
