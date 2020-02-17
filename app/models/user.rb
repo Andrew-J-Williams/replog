9be7c65aa0d5b5b39a06f1b594322337b5e1d7cc
Andrew-J-Williams <1williamsaj@gmail.com>
2020-02-17 17:29:25 -0600

    updated validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
     has_secure_password
 
     validates :username, :email, :password, presence: true
-    validates :email, uniqueness: true
+    validates :username, :email, uniqueness: true
 end
